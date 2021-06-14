575eb76a85d10839184d96580c79fe635bacc408
luke gilchrist <lukegilchrist69@hotmail.com>
2021-06-14 12:45:12 +1200

    Fetching coin api data

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,13 @@
-let promise = fetch('https://api.coingecko.com/api/v3/ping', {}).then(response => console.log(response));
+
+// Get list of coins from API
+let promise1 = fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=market_cap_desc&per_page=100&page=1&sparkline=false', {})
+
+// Check if api data is being fetched
+promise1.then((data) => {
+    if (data.ok) {
+        console.log("Success");
+    } else {
+        console.log("ERROR")
+    }
+    // expected output: "Success!"
+});
