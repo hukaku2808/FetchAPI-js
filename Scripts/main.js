f6174a290b14a806bb27e598c533f43a7dac514a
luke gilchrist <lukegilchrist69@hotmail.com>
2021-06-14 16:26:40 +1200

    UPDATE

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,13 @@
+// Fetch crypto coins from coingecko api
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&market_cap_desc')
+    // Check if api data is being fetched
     .then(response => {
         response.json().then(data => {
             console.log(data);
             if (data.length > 0) {
+                // variable
                 var temp = "";
+                // name table colums to display from api 
                 data.forEach(itemData => {
                     temp += "<tr>";
                     temp += "<td>" + "<img width='50px' height='50px'src=" + itemData.image + " </td>";
@@ -14,73 +18,16 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=mark
                     temp += "<td>" + '$' + itemData.market_cap + "</td>";
                     "</tr>";
                 });
-
+                //  Get id from table in HTML document and display api data
                 document.getElementById('data').innerHTML = temp;
+                // if reciving data fromdisplay connected message on console
+                return console.log("successfully loaded data")
+            }
+            else {
+                // if data not found return a error message on console 
+                return console.log("Cannot Find Connection to API");
+
             }
         }
-
-
         )
-    }
-    )
-
-
-
-
-//        if (response.ok) {
-//             return response.json();
-//         } else {
-//             throw new Error("NETWORK RESPONSE ERROR");
-//         }
-//     })
-//     .then(data => {
-//         console.log(data);
-//         displayCoingecko(data)
-//     })
-//     .catch((error) => console.error("FETCH ERROR:", error));
-// function displayCoingecko(data) {
-
-//     const coingecko = data[0]
-//     const coingeckoDiv = document.getElementById("coingecko");
-//     const coingeckoName = coingecko.name;
-//     const heading = document.createElement("h1");
-//     heading.innerHTML = coingeckoName;
-//     coingeckoDiv.appendChild(heading);
-//     const coingeckoImg = document.createElement("img");
-//     coingeckoImg.src = coingecko.image;
-//     document.body.style.backgroundImage = "url('" + coingecko.image + "')";
-// }
-
-
-// Get list of coins from API
-/*
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h', {})
-    // Check if api data is being fetched
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("NETWORK RESPONSE ERROR");
-        }
-    })
-    .then(data => {
-        console.log(data);
-        displayCoingecko(data)
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
-*/
-
-
-// showCoins = coins => {
-//     const coinsDiv = document.querySelector('#coingeckocoins');
-
-//     coins.forEach(coin => {
-
-//         const coinElement = document.createElement('p');
-
-//         coinElement.innerText = `Coin Name: ${coin.symbol}`;
-
-//         coinsDiv.append(coinElement);
-//     });
-
-// }
+    }).catch((error) => console.error("FETCH ERROR:", error));
